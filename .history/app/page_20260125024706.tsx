2810b5cd874a6b9d821ff7f1100d11c048960890
tusiimegeorge <tusiimegeorgetrevour12@gmail.com>
2026-01-25 02:48:18 +0300

    xo

--- .history/app/page_20260125024706.tsx
+++ .history/app/page_20260125024706.tsx
@@ -121,7 +121,7 @@ export default function MenuPage() {
     { name: "Martel V.S.O.P", bottlePrice: "UGX 750,000" },
     { name: "Hennessy V.S.O.P", bottlePrice: "UGX 800,000", totPrice: "UGX 25,000"},
     { name: "Hennessy XO", bottlePrice: "UGX 2,600,000" },
-     { name: "Martel XO", bottlePrice: "UGX 3,000,000" },
+     { name: "Hennessy XO", bottlePrice: "UGX 2,000,000" },
     //{ name: "Martel Blue Swift", bottlePrice: "UGX 650,000" },
     //{ name: "Courvoisier Vs", bottlePrice: "UGX 650,000" },
   ]
